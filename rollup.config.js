a6a2b8aaaf7b643e0b1d5cd61bf378752a039bdd
dragosguta <contact.dragosguta@gmail.com>
2021-08-07 12:52:20 -0700

    [FIX]: Update rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
 import json from '@rollup/plugin-json';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 
 import { terser } from 'rollup-plugin-terser';
 
