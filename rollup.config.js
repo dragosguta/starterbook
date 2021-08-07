37d55eb9f00a6b93a67e5c555a721a7222034f13
dragosguta <contact.dragosguta@gmail.com>
2021-08-07 11:46:44 -0700

    [FIX]: Rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
+import json from '@rollup/plugin-json';
+import commonjs from 'rollup-plugin-commonjs';
 
 import { terser } from 'rollup-plugin-terser';
 
@@ -38,6 +40,8 @@ export default [
       external(),
       resolve(),
       terser(),
+      json(),
+      commonjs(),
     ],
   },
 ];
