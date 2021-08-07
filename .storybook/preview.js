29b91e978420b727d876e7a0ddcd4577d82bbbd9
dragosguta <contact.dragosguta@gmail.com>
2021-08-07 10:32:17 -0700

    [FEAT]: Update storybook with global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 import { ThemeProvider } from 'theme-ui';
+import { Global, css } from '@emotion/react';
+
 import theme from '../src/theme';
 
 export const parameters = {
@@ -14,6 +16,34 @@ export const parameters = {
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          @font-face {
+            font-family: relative-book-pro;
+            font-style: normal;
+            font-weight: 400;
+            src: url(/relative-book-pro.woff2);
+          }
+          @font-face {
+            font-family: relative-medium-pro;
+            font-style: normal;
+            font-weight: 400;
+            src: url(/relative-medium-pro.woff2);
+          }
+          @font-face {
+            fontfamily: relative-mono-11-pitch-pro;
+            fontstyle: normal;
+            fontweight: 400;
+            src: url(/relative-mono-11-pitch-pro.woff2);
+          }
+          @font-face {
+            fontfamily: relative-faux-book-pro;
+            fontstyle: normal;
+            fontweight: 400;
+            src: url(/relative-faux-book-pro.woff2);
+          }
+        `}
+      />
       <div style={{ margin: '3rem' }}>
         <Story />
       </div>
