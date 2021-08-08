5363f774ab929d5f589b150122b9c7e275a0eca9
dragosguta <contact.dragosguta@gmail.com>
2021-08-07 18:55:24 -0700

    [FIX]: Minor edits to elements

--- src/components/Icon/List.tsx
+++ src/components/Icon/List.tsx
@@ -17,7 +17,7 @@ export function List({ id, csx }: { id?: string; csx?: ThemeUIStyleObject }) {
       }}
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
       />
     </ThemedBox>
